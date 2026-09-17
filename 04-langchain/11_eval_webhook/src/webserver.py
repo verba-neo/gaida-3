0658d0ec8be95b020d334148276b729c4cfb9579
verba-neo <verba.it.corp@gmail.com>
2026-09-17 11:05:42 +0900

    gg

--- 04-langchain/11_eval_webhook/src/webserver.py
+++ 04-langchain/11_eval_webhook/src/webserver.py
@@ -6,13 +6,32 @@ uv add fastapi python-telegram-bot
 import os
 from dotenv import load_dotenv
 
-from fastapi import FastAPI
-from telegram import Bot
+from fastapi import FastAPI, Request, BackgroundTasks, Header
+from telegram import Bot, Update
 
 load_dotenv()
 
 app = FastAPI()
+
 bot = Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))
 
-print(bot.get_updates())
-# bot.send_message()
+@app.post('/telegram/webhook')  # localhost:2024/telegram/webhook
+async def telegram(
+    request: Request,
+    background_tasks: BackgroundTasks,
+):
+
+    payload = await request.json()
+    update = Update.de_json(payload, bot)
+    message = update.message
+
+    print('-------------------------------')
+    print(message.chat.id, message.text)
+    print('-------------------------------')
+
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text=message.text
+    )
+
+    return {'ok': True}
